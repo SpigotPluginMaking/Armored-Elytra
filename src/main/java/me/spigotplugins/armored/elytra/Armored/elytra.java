3b9426c021b946b1b882578c1d1c4f7ebbd77d89
JD3006 <48618081+JD3006@users.noreply.github.com>
2022-09-02 17:36:16 +0800

    yes

--- src/main/java/me/spigotplugins/armored/elytra/Armored/elytra.java
+++ src/main/java/me/spigotplugins/armored/elytra/Armored/elytra.java
@@ -7,7 +7,7 @@ public final class elytra extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        System.out.println("yes");
     }
 
     @Override
